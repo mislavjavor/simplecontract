0a86f53313ee63bc847bbc09b80afc4faac4eab6
mjr <mislav.javor@outlook.com>
2018-04-27 13:12:54 +0200

    Update

---
+++ contracts/SplitMoney.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.21;
+
+import "zeppelin-solidity/contracts/tokens/ERC20/StandardToken.sol";
+
+
+contract SplitMoney is StandardToken {
+
+    string public symbol = "STM";
+    string public name = "SplitMoney";
+    uint8 public decimals = 18;
+
+    function SplitMoney() public {
+        balances[msg.sender] = 1000000 * (10 ** uint(decimals));
+        totalSupply_ = 1000000 * (10 ** uint(decimals));
+    }
+
+}
